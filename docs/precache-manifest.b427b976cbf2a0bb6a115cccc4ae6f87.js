3140ed1350cb95ba8551d6e4d72194b444c4c1d0
Estefano Aguirre <estefanoao03@gmail.com>
2021-04-30 01:01:25 -0500

    docs agregado

---
+++ docs/precache-manifest.b427b976cbf2a0bb6a115cccc4ae6f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd53e9828b5d0c8b43d64d87d8088627",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dad5b2816b04feb7e437",
+    "url": "/static/css/main.ec1134a6.chunk.css"
+  },
+  {
+    "revision": "21128ddbf1ff63946978",
+    "url": "/static/js/2.31a10122.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.31a10122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad5b2816b04feb7e437",
+    "url": "/static/js/main.8c7083cb.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
